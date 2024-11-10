0377d0da54c94184a902fc077ce1304125eeb4d6
kingstar <admin@baks.dev>
2024-11-11 00:15:38 +0300

    Baks Development

--- Api/Review/GetListReviews/AvitoReviewDTO.php
+++ Api/Review/GetListReviews/AvitoReviewDTO.php
@@ -1,17 +1,17 @@
 <?php
 /*
  *  Copyright 2024.  Baks.dev <admin@baks.dev>
- *
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,7 +27,7 @@ namespace BaksDev\Avito\Support\Api\Review\GetListReviews;
 
 use DateTimeImmutable;
 
-final  class AvitoReviewDTO
+final class AvitoReviewDTO
 {
 
     /** ID отзыва */
@@ -68,20 +68,41 @@ final  class AvitoReviewDTO
 
         if(!empty($images))
         {
+            $result[0] = '<br>';
+
             foreach($images as $image)
             {
-                foreach($image['sizes'] as $size)
+                $sizes = $image['sizes'];
+
+                // сортируем массив по площади изображения (первым будем с максимальным разрешением)
+                usort($sizes, function($a, $b) {
+                    return $this->getArea($b['size']) <=> $this->getArea($a['size']);
+                });
+
+                $size = current($sizes);
+
+                if(!isset($result[$image['number']]))
                 {
-                    $result[] = sprintf(
-                        '<img src="%s" title="%s"/>', $size['url'], $image['number']
+                    $result[$image['number']] = sprintf(
+                        '<a href="%s" target="_blank"><img data-src="%s" class="lazy" title="%s" style="max-height: 50px;" /></a>',
+                        $size['url'],
+                        $size['url'],
+                        $image['number']
                     );
                 }
             }
         }
 
-
         return !empty($result) ? $text.' '.implode(' ', $result) : trim($text);
     }
+
+
+    function getArea($size)
+    {
+        list($width, $height) = explode('x', $size);
+        return $width * $height; // Площадь
+    }
+
 
     public function getId(): int
     {
