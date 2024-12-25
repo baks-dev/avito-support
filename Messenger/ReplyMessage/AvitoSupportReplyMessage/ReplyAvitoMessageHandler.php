d5d491acbc7dee78a381ddefb36bfc2d752ebfb6
kingstar <admin@baks.dev>
2024-12-25 15:54:12 +0300

    Baks Development

--- Messenger/ReplyMessage/AvitoSupportReplyMessage/ReplyAvitoMessageHandler.php
+++ Messenger/ReplyMessage/AvitoSupportReplyMessage/ReplyAvitoMessageHandler.php
@@ -1,4 +1,25 @@
 <?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
 
 declare(strict_types=1);
 
@@ -7,6 +28,8 @@ namespace BaksDev\Avito\Support\Messenger\ReplyMessage\AvitoSupportReplyMessage;
 
 use BaksDev\Avito\Support\Api\Messenger\Post\SendMessage\AvitoSendMessageRequest;
 use BaksDev\Avito\Support\Types\ProfileType\TypeProfileAvitoMessageSupport;
+use BaksDev\Core\Messenger\MessageDelay;
+use BaksDev\Core\Messenger\MessageDispatchInterface;
 use BaksDev\Support\Entity\Event\SupportEvent;
 use BaksDev\Support\Messenger\SupportMessage;
 use BaksDev\Support\Repository\SupportCurrentEvent\CurrentSupportEventInterface;
@@ -25,6 +48,7 @@ final class ReplyAvitoMessageHandler
     public function __construct(
         private AvitoSendMessageRequest $messageRequest,
         private CurrentSupportEventInterface $currentSupportEvent,
+        private readonly MessageDispatchInterface $messageDispatcher,
         LoggerInterface $avitoSupportLogger,
     )
     {
@@ -84,10 +108,19 @@ final class ReplyAvitoMessageHandler
         $ticket = $SupportInvariableDTO->getTicket();
 
         /** Отправка сообщения */
-        $this->messageRequest
+        $send = $this->messageRequest
             ->profile($SupportInvariableDTO->getProfile())
             ->message($lastMessage->getMessage())
             ->send($ticket);
 
+        if(false === $send)
+        {
+            /** Пробуем отправить отправить через минуту */
+            $this->messageDispatcher->dispatch(
+                message: $message,
+                stamps: [new MessageDelay('1 minutes')],
+                transport: 'avito-support'
+            );
+        }
     }
 }
