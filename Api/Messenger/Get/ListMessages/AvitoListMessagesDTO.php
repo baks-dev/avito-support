f059a0cd98c35dd773caf6e1bce6e2003f59be40
admin <admin@baks.dev>
2025-05-20 01:34:27 +0300

    BaksDevelopment

--- Api/Messenger/Get/ListMessages/AvitoListMessagesDTO.php
+++ Api/Messenger/Get/ListMessages/AvitoListMessagesDTO.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -85,7 +85,7 @@ final class AvitoListMessagesDTO
     private function image($image): string
     {
         $small = sprintf('<img src="%s" />', $image['sizes']['32x32']);
-        $large = sprintf('<a href="%s" class="ms-3" target="_blank" />Открыть полное фото<a/>', $image['sizes']['1280x960']);
+        $large = sprintf('<a href="%s" class="ms-3" target="_blank" />Открыть полное фото</a>', $image['sizes']['1280x960']);
 
         return $small.' '.$large;
     }
